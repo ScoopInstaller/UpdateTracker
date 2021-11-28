0f3087467d657611ac20cb7f2e413998648a5908
unknown <issaclin32@gmail.com>
2021-11-28 19:21:57 +0800

    update

--- ryzen-controller/run.sh
+++ ryzen-controller/run.sh
@@ -2,13 +2,13 @@
 xml="<?xml version=\"1.0\"?>"
 xml+="<time>$(echo $(TZ=UTC-8 date '+%Y-%m-%d %H:%M:%S'))</time>"
 
-checkver_page="https://gitlab.com/ryzen-controller-team/ryzen-controller/-/tags?sort=name_desc&format=atom"
-checkver_page_content=$(curl $checkver_page)
+checkver_url="https://gitlab.com/ryzen-controller-team/ryzen-controller/-/tags?sort=name_desc&format=atom"
+checkver_url_content=$(curl checkver_url)
 
 # ryzen-controller/-/tags/2.5.4
-if [checkver_page !=~ "ryzen-controller/-/tags/([\\d.]+)"]
+if [$checkver_url_content !=~ "ryzen-controller/-/tags/([\\d.]+)"]
 then
-    echo "Cannot match regex on $checkver_page"
+    echo "Cannot match regex on checkver_url"
     exit 1
 fi
 echo $BASH_REMATCH[1]
